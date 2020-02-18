3a6a3b39ebf7647ad12a2eebe1562dc29eee792f
ShannenHarper <shannen_harper@hotmail.co.uk>
2020-02-18 06:09:57 +0000

    chapter 7

--- templates/rango/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from rango.models import Page, Category
-
-class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
-                           help_text="Please enter the category name.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    likes = forms.IntegerField(widget)
